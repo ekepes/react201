7c1b57a92dafb6c94a3e2b158db47c2214999c96
Eric Kepes <eric@kepes.net>
2017-07-13 20:21:51 -0400

    Our first component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import './App.css';
+import HelloWorld from './HelloWorld.js';
 
 const App = () => {
-    return (<div className="App">Hello World!</div>);
-}
+    return (
+        <div className="App">
+            <HelloWorld/>
+            <HelloWorld/>
+        </div>
+    );
+};
 
 export default App;
