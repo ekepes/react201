dab77c457337021368f884cb4573f028e0261220
Eric Kepes <eric@kepes.net>
2017-07-29 13:52:52 -0400

    Turned the list into a component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import './App.css';
-import HelloWorld from './HelloWorld.js';
+import HelloWorldList from './HelloWorldList.js';
 
 const App = () => {
     return (
         <div className="App">
-            <HelloWorld name="Jim"/>
-            <HelloWorld name="Sally"/>
+            <HelloWorldList/>
         </div>
     );
 };
---
+++ src/HelloWorldList.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import HelloWorld from './HelloWorld';
+import './HelloWorldList.css';
+
+class HelloWorldList extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { greetings: ['Jim', 'Sally', 'Bob'] };
+    }
+
+    renderGreetings() {
+        return this.state.greetings.map(name => (
+            <HelloWorld key={name} name={name}/>
+        ));
+    }
+
+    render() {
+        return (
+            <div className="HelloWorldList">
+                {this.renderGreetings()}
+            </div>
+        );
+    }
+}
+export default HelloWorldList;
